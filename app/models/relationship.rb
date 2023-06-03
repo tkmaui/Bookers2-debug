37dfcd9d1fafef81798316d89e9eeebbf92cdebc
tkmaui <129600101+tkmaui@users.noreply.github.com>
2023-06-03 02:56:30 +0000

    Finish

---
+++ app/models/relationship.rb
@@ -0,0 +1,16 @@
+class Relationship < ApplicationRecord
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+end
